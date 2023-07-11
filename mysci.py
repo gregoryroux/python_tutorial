3c1f0c75479a2a05333aa593b36fd03f450e3ae5
ROUX Gregory <rouxg@sxrecyf2>
2023-07-11 11:10:47 +0200

    Parsing select time-series

--- mysci.py
+++ mysci.py
@@ -1,41 +1,22 @@
-# Read the data file
-#filename = "data/wxobs20170821.txt"
-#datafile = open(filename, 'r')
-#print(datafile.readline())
-#data = datafile.read()
-
-data=[]
-#with open(filename, 'r') as datafile:
-#   data = datafile.read()
+# Initialize my data variable
+data = {'date': [],
+  'time': [],
+  'tempout': []}
 
 # Read and parse the data file
 filename = "data/wxobs20170821.txt"
 with open(filename, 'r') as datafile:
 
- # Read the first three lines (header)
- for _ in range(3):
-    datafile.readline()
+   # Read the first three lines (header)
+   for _ in range(3):
+      datafile.readline()
 
- # Read and parse the rest of the file
- for line in datafile:
-    datum = line.split()
-    data.append(datum)
-
-# DEBUG
-#for datum in data:
-#   print(datum)
-# DEBUG
-#print(data[0])
-print(data[8])
-#print(data[-1])
-#for datum in data[0:10]:
-#    print(datum)
-
-print(data[8][4])
-print(data[8][:5])
-print(data[8][::2])
+   # Read and parse the rest of the file
+   for line in datafile:
+      split_line = line.split()
+      data['date'].append(split_line[0])
+      data['time'].append(split_line[1])
+      data['tempout'].append(split_line[2])
 
 # DEBUG
-#print(data)
-#print(type(data))
-
+print(data['time'])
